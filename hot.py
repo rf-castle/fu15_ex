7a92ed685d7a481f6508914b2d12d840fc904219
rf-castle <kouta.tashiro.2000@gmail.com>
2020-07-29 15:31:26 +0900

    HoT: Initial revision

---
+++ hot.py
@@ -0,0 +1,12 @@
+import random
+heads = tails = 0
+print("Tossing a coin...")
+for n in range(3):
+    print(f"Round {n}: ", end="")
+    if random.random() < 0.5:
+        print("Heads")
+        heads += 1
+    else:
+        print("Tails")
+        tails += 1
+print(f"Heads: {heads}, Tails: {tails}")
